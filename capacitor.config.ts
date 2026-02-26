ed8601d870488e99aaaa7f90a4edf895de7f666d
Ashwin Girish <ashwingirish@Ashwins-MacBook-Air.local>
2026-02-26 22:54:55 +0000

    chore: auto-sync (2026-02-26 22:54:55)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mrash.spottoplot',
+  appName: 'Spot-to-Plot',
+  webDir: 'dist'
+};
+
+export default config;
